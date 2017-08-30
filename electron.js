6e00605d1f6bea8e228d520521c5316ed983772a
Cristi Constantin <cristi.constantin@live.com>
2017-08-30 17:43:30 +0300

    Minimal build system

--- electron.js
+++ electron.js
@@ -7,7 +7,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 760, height: 600, backgroundColor: '#0000009f'})
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),

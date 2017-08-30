13af02a99f11623de9683ed65b55cea1611d0815
Cristi Constantin <cristi.constantin@live.com>
2017-08-30 23:54:43 +0300

    Minimal working example

--- electron.js
+++ electron.js
@@ -7,7 +7,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 760, height: 600, backgroundColor: '#0000009f'})
+  win = new BrowserWindow({width: 760, height: 600, backgroundColor: '#FFF8DC'})
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
